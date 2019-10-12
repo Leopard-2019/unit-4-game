367f50f1542b8652b5d1d7f4e6021e812839350f
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2019-10-12 19:45:40 -0400

    comments have been added to the game.js file, also small editing was done to style.css file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,15 @@ var counterWins = 0;
 var counterLosses = 0;
 var n1Score = 0;
 
-// a randon funtion is assigned to the number that need to be matched
+// a randon funtion is assigned to the number that need to be matched (this number varies between 18-120)
+
 var mNumber = Math.floor(Math.random() * (102)) + 12;
 var sum = 0;
 
 
 // a randon funtion is assigned to each of the crystal button, after
-// a number is assigned to each of them, they will remain fixed until the number to be matched 
-// is equalled or exceeded
+// a number between 1 and 12 is assigned to each of them, they will remain fixed until the number to be matched 
+// is matched or exceeded
 
 $("#button-1").val(function () {
     return Math.floor(Math.random() * (11)) + 1;
@@ -33,6 +34,11 @@ $("#mNumber").text(mNumber);
 $("#nWins").text(counterWins);
 $("#nLosses").text(counterLosses);
 $("#nScore").text(n1Score);
+
+// the main engine function, the "if" check if the variable sum (the number results of the sum by clicking the crytal buttons)
+// is equal to the random number that need to be matched, if the variable "mNumber" is matched (sum=nNumber), then a wins is counted
+// the "else if" basically check if the number to be matched is exceed, then a losses is counted
+// the "else" basically keeps adding number to the variable "sum" when clicking the crystal buttons,etc.
 
 $(".number").on("click", function () {
     if (sum == mNumber) {
@@ -56,6 +62,11 @@ $(".number").on("click", function () {
         $("#sSum").text(sum);
     }
 });
+
+// this function basically allow the player to play again  while keeping the number of wins, losses, 
+// and the total score (wins - losses) obtained in previous plays as far as the whole game is not
+// reset when hitting the refresh button
+
 $("#rStart").on("click", function RStart() {
 
     mNumber = Math.floor(Math.random() * (102)) + 12;
